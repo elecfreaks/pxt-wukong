6ce2ebe09fafce478570ec81358b478b2b8309d9
LION-YAN\Lion <Lion_yan@outlook.com>
2019-08-20 12:05:05 +0800

    no message

--- main.ts
+++ main.ts
@@ -211,5 +211,361 @@ namespace wuKong {
         buf[3] = 0;
         pins.i2cWriteBuffer(board_address, buf);
     }
+    /***************************************************Mecanum wheel car******************************************/
+    let LeftFront_def = ServoList.S0
+    let LeftRear_def = ServoList.S1
+    let RightFront_def = ServoList.S2
+    let RightRear_def = ServoList.S3
+    /**
+    * ServoList
+    */
+    export enum WheelList {
+        //% block="Left Front" 
+        LeftFront_def,
+        //% block="Left Rear"
+        LeftRear_def,
+        //% block="Right Front"
+        RightFront_def,
+        //% block="Right Rear"
+        RightRear_def
+    }
+    export enum RunList {
+        //% block="↖" 
+        LeftFront,
+        //% block="↑"
+        Front,
+        //% block="↗"
+        RightFront,
+        //% block="←"
+        left,
+        //% block="P" 
+        stop,
+        //% block="→"
+        right,
+        //% block="↙"
+        LeftRear,
+        //% block="↓"
+        rear,
+        //% block="↘"
+        RightRear
+    }
+    export enum TurnList {
+        //% block="Left" 
+        Left,
+        //% block="Right"
+        Right
+    }
+    /**
+    * TODO: Set Mecanum wheel car 
+    * @param LeftFront A servo in the ServoList , eg: ServoList.S0
+    * @param LeftRear A servo in the ServoList , eg: ServoList.S1
+    * @param RightFront A servo in the ServoList , eg: ServoList.S2
+    * @param RightRear A servo in the ServoList , eg: ServoList.S3
+    */
+    //% block="Set Mecaum wheel|Left Front %LeftFront|Left Rear %LeftRear|Right Front %RightFront|Right Rear %RightRear"
+    //% subcategory=Mecanum
+    export function mecanumWheel(LeftFront: ServoList, LeftRear: ServoList, RightFront: ServoList, RightRear: ServoList): void {
+        LeftFront_def = LeftFront
+        LeftRear_def = LeftRear
+        RightFront_def = RightFront
+        RightRear_def = RightRear
+    }
 
+    /**
+    * TODO: Set servo speed
+    */
+    //% block="Set %wheel wheel speed to %speed"
+    //% subcategory=Mecanum
+    export function mecanumSpeed(wheel: WheelList, speed: number): void {
+        let buf = pins.createBuffer(4)
+        if (wheel < 2) {
+            if (speed == 0) {
+                speed = 89
+            }
+            else {
+                if (speed > 0) {
+                    speed = Math.map(speed, 1, 100, 90, 180)
+                }
+                if (speed < 0) {
+                    speed = speed * -1
+                    speed = Math.map(speed, 1, 100, 90, 0)
+                }
+            }
+        }
+        else {
+            if (speed == 0) {
+                speed = 89
+            }
+            else {
+                if (speed > 0) {
+                    speed = Math.map(speed, 1, 100, 90, 0)
+                }
+                if (speed < 0) {
+                    speed = speed * -1
+                    speed = Math.map(speed, 1, 100, 90, 180)
+                }
+            }
+        }
+
+        switch (wheel) {
+            case 0:
+                if (LeftFront_def == 0) {
+                    buf[0] = 0x03;
+                    break;
+                }
+                if (LeftFront_def == 1) {
+                    buf[0] = 0x04;
+                    break;
+                }
+                if (LeftFront_def == 2) {
+                    buf[0] = 0x05;
+                    break;
+                }
+                if (LeftFront_def == 3) {
+                    buf[0] = 0x06;
+                    break;
+                }
+                if (LeftFront_def == 4) {
+                    buf[0] = 0x07;
+                    break;
+                }
+                if (LeftFront_def == 5) {
+                    buf[0] = 0x08;
+                    break;
+                }
+                if (LeftFront_def == 6) {
+                    buf[0] = 0x09;
+                    break;
+                }
+                if (LeftFront_def == 7) {
+                    buf[0] = 0x10;
+                    break;
+                }
+            case 1:
+                if (LeftRear_def == 0) {
+                    buf[0] = 0x03;
+                    break;
+                }
+                if (LeftRear_def == 1) {
+                    buf[0] = 0x04;
+                    break;
+                }
+                if (LeftRear_def == 2) {
+                    buf[0] = 0x05;
+                    break;
+                }
+                if (LeftRear_def == 3) {
+                    buf[0] = 0x06;
+                    break;
+                }
+                if (LeftRear_def == 4) {
+                    buf[0] = 0x07;
+                    break;
+                }
+                if (LeftRear_def == 5) {
+                    buf[0] = 0x08;
+                    break;
+                }
+                if (LeftRear_def == 6) {
+                    buf[0] = 0x09;
+                    break;
+                }
+                if (LeftRear_def == 7) {
+                    buf[0] = 0x10;
+                    break;
+                }
+            case 2:
+                if (RightFront_def == 0) {
+                    buf[0] = 0x03;
+                    break;
+                }
+                if (RightFront_def == 1) {
+                    buf[0] = 0x04;
+                    break;
+                }
+                if (RightFront_def == 2) {
+                    buf[0] = 0x05;
+                    break;
+                }
+                if (RightFront_def == 3) {
+                    buf[0] = 0x06;
+                    break;
+                }
+                if (RightFront_def == 4) {
+                    buf[0] = 0x07;
+                    break;
+                }
+                if (RightFront_def == 5) {
+                    buf[0] = 0x08;
+                    break;
+                }
+                if (RightFront_def == 6) {
+                    buf[0] = 0x09;
+                    break;
+                }
+                if (RightFront_def == 7) {
+                    buf[0] = 0x10;
+                    break;
+                }
+            case 3:
+                if (RightRear_def == 0) {
+                    buf[0] = 0x03;
+                    break;
+                }
+                if (RightRear_def == 1) {
+                    buf[0] = 0x04;
+                    break;
+                }
+                if (RightRear_def == 2) {
+                    buf[0] = 0x05;
+                    break;
+                }
+                if (RightRear_def == 3) {
+                    buf[0] = 0x06;
+                    break;
+                }
+                if (RightRear_def == 4) {
+                    buf[0] = 0x07;
+                    break;
+                }
+                if (RightRear_def == 5) {
+                    buf[0] = 0x08;
+                    break;
+                }
+                if (RightRear_def == 6) {
+                    buf[0] = 0x09;
+                    break;
+                }
+                if (RightRear_def == 7) {
+                    buf[0] = 0x10;
+                    break;
+                }
+        }
+        buf[1] = speed;
+        buf[2] = 0;
+        buf[3] = 0;
+        pins.i2cWriteBuffer(board_address, buf);
+    }
+    /**
+   * TODO: Set car runs direction
+   */
+    //% block="Set Mecanum car runs direction %type"
+    //% subcategory=Mecanum
+    //% type.fieldEditor="gridpicker"
+    //% type.fieldOptions.columns=3
+    export function mecanumRun(type: RunList): void {
+        switch (type) {
+            case 0:
+                setServoAngel(LeftFront_def, 90)
+                setServoAngel(LeftRear_def, 180)
+                setServoAngel(RightFront_def, 0)
+                setServoAngel(RightRear_def, 90)
+                break;
+            case 1:
+                setServoAngel(LeftFront_def, 180)
+                setServoAngel(LeftRear_def, 180)
+                setServoAngel(RightFront_def, 0)
+                setServoAngel(RightRear_def, 0)
+                break;
+            case 2:
+                setServoAngel(LeftFront_def, 180)
+                setServoAngel(LeftRear_def, 90)
+                setServoAngel(RightFront_def, 90)
+                setServoAngel(RightRear_def, 0)
+                break;
+            case 3:
+                setServoAngel(LeftFront_def, 0)
+                setServoAngel(LeftRear_def, 180)
+                setServoAngel(RightFront_def, 0)
+                setServoAngel(RightRear_def, 180)
+                break;
+            case 4:
+                setServoAngel(LeftFront_def, 90)
+                setServoAngel(LeftRear_def, 90)
+                setServoAngel(RightFront_def, 90)
+                setServoAngel(RightRear_def, 90)
+                break;
+            case 5:
+                setServoAngel(LeftFront_def, 180)
+                setServoAngel(LeftRear_def, 0)
+                setServoAngel(RightFront_def, 180)
+                setServoAngel(RightRear_def, 0)
+                break;
+            case 6:
+                setServoAngel(LeftFront_def, 0)
+                setServoAngel(LeftRear_def, 90)
+                setServoAngel(RightFront_def, 90)
+                setServoAngel(RightRear_def, 180)
+                break;
+            case 7:
+                setServoAngel(LeftFront_def, 0)
+                setServoAngel(LeftRear_def, 0)
+                setServoAngel(RightFront_def, 180)
+                setServoAngel(RightRear_def, 180)
+                break;
+            case 8:
+                setServoAngel(LeftFront_def, 90)
+                setServoAngel(LeftRear_def, 180)
+                setServoAngel(RightFront_def, 0)
+                setServoAngel(RightRear_def, 90)
+                break;
+        }
+    }
+    /**
+    * TODO: Set Mecanum car Stop
+    */
+    //% block="Set Mecanum car Stop"
+    //% subcategory=Mecanum
+    export function mecanumStop(): void {
+        setServoAngel(LeftFront_def, 90)
+        setServoAngel(LeftRear_def, 90)
+        setServoAngel(RightFront_def, 90)
+        setServoAngel(RightRear_def, 90)
+    }
+    /**
+   * TODO: Set car spin 
+   */
+    //% block="Set Mecanum car spin %Turn"
+    //% subcategory=Mecanum
+    //% Turn.fieldEditor="gridpicker"
+    //% Turn.fieldOptions.columns=2
+    export function mecanumSpin(Turn: TurnList): void {
+        switch (Turn) {
+            case 0:
+                setServoAngel(LeftFront_def, 0)
+                setServoAngel(LeftRear_def, 0)
+                setServoAngel(RightFront_def, 0)
+                setServoAngel(RightRear_def, 0)
+                break;
+            case 1:
+                setServoAngel(LeftFront_def, 180)
+                setServoAngel(LeftRear_def, 180)
+                setServoAngel(RightFront_def, 180)
+                setServoAngel(RightRear_def, 180)
+                break;
+        }
+    }
+    /**
+   * TODO: Set car drift
+   */
+    //% block="Set Mecanum car drift %Turn"
+    //% subcategory=Mecanum
+    //% Turn.fieldEditor="gridpicker"
+    //% Turn.fieldOptions.columns=2
+    export function mecanumDrift(Turn: TurnList): void {
+        switch (Turn) {
+            case 0:
+                setServoAngel(LeftFront_def, 70)
+                setServoAngel(LeftRear_def, 180)
+                setServoAngel(RightFront_def, 70)
+                setServoAngel(RightRear_def, 180)
+                break;
+            case 1:
+                setServoAngel(LeftFront_def, 110)
+                setServoAngel(LeftRear_def, 0)
+                setServoAngel(RightFront_def, 110)
+                setServoAngel(RightRear_def, 0)
+                break;
+        }
+    }
 }
