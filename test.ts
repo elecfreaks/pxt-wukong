1c1c997e2c3345f7d4c1959e8ec4d3aa7157aef9
uio64 <2692571183@qq.com>
2026-03-12 10:52:30 +0800

    Correct servo motor type parameter error

--- test.ts
+++ test.ts
@@ -5,7 +5,7 @@ input.onButtonPressed(Button.AB, function () {
     wuKong.stopAllMotor()
 })
 input.onButtonPressed(Button.B, function () {
-    wuKong.setServoAngle(wuKong.ServoTypeList._180,wuKong.ServoList.S0, 360)
+    wuKong.setServoAngle(wuKong.ServoTypeList._360,wuKong.ServoList.S0, 360)
     wuKong.setServoAngle(wuKong.ServoTypeList._180,wuKong.ServoList.S2, 180)
     wuKong.setServoAngle(wuKong.ServoTypeList._180,wuKong.ServoList.S4, 90)
     wuKong.setServoAngle(wuKong.ServoTypeList._180,wuKong.ServoList.S6, 0)
